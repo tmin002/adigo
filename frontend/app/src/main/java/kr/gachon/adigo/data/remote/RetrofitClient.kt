31ba0e4983ae230c61474718a3756ed34afd194c
jihoseo <ssohye@icloud.com>
2025-03-21 14:36:39 +0900

    etc: 레트로핏을 이용한 httpclient 구현체 구현 - API RESPONSE,REQUEST모델 생성

--- frontend/app/src/main/java/kr/gachon/adigo/data/remote/RetrofitClient.kt
+++ frontend/app/src/main/java/kr/gachon/adigo/data/remote/RetrofitClient.kt
@@ -6,25 +6,44 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 // 3. RetrofitClient.kt
-object ApiClient {
-    private const val BASE_URL = "https://api.adigo.kr/"
+object httpClient {
+    private const val BASE_URL = "https://adigo.site/api/"
 
     fun create(tokenManager: TokenManager): ApiService {
-        val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor { chain ->
-                val requestBuilder = chain.request().newBuilder()
-                tokenManager.getJwtToken()?.let { token ->
-                    requestBuilder.addHeader("Authorization", "Bearer $token")
-                }
-                chain.proceed(requestBuilder.build())
-            }
-            .build()
 
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiService::class.java)
+
+        //jwt토큰이 기기에 저장되어있지 않으면 일반 retrofit 인스턴스를 돌려줌
+        if(tokenManager.getJwtToken()==null){
+            var okHttpClient = OkHttpClient.Builder().build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiService::class.java)
+
+        }else{  //이미 jwt토큰이 기기에 저장되어있으면 http헤더에 jwt토큰 정보 추가한 retrofit 인스턴스를 돌려줌
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val requestBuilder = chain.request().newBuilder()
+                    tokenManager.getJwtToken()?.let { token ->
+                        requestBuilder.addHeader("Authorization", "Bearer $token")
+                    }
+                    chain.proceed(requestBuilder.build())
+                }
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiService::class.java)
+        }
+
+
+
+
     }
 }
