38918b234b5add07f76947183ef8800ae48f4cc8
jihoseo <ssohye@icloud.com>
2025-03-21 14:36:39 +0900

    레트로핏을 이용한 httpclient 구현체 구현
    API RESPONSE,REQUEST모델 생성

--- frontend/app/src/main/java/kr/gachon/adigo/data/remote/ApiService.kt
+++ frontend/app/src/main/java/kr/gachon/adigo/data/remote/ApiService.kt
@@ -4,14 +4,13 @@ import kr.gachon.adigo.data.model.LoginRequest
 import kr.gachon.adigo.data.model.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
-import okhttp3.ResponseBody
 
 
 
 
 interface ApiService {
+
     @POST("auth/login")
     suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
 }
