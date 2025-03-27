315dcb9f9a5adfcc7066c1cfddf44a15ab9cd389
tmin002 <ssh9930@gmail.com>
2025-03-27 12:41:51 +0900

    add: ManagedModel 및 공통 모델들 추가

---
+++ frontend/app/src/main/java/kr/gachon/adigo/data/local/entity/UserEntity.kt
@@ -0,0 +1,13 @@
+package kr.adigo.adigo.database.entity
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import java.net.URL
+
+class UserEntity: RealmObject {
+    @PrimaryKey
+    lateinit var id: String;
+    lateinit var name: String;
+    lateinit var profileImageURL: URL;
+    lateinit var phoneNumber: String;
+}
