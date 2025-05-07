9bd4d255c740348fb7e2fc38e51a9499c4f8ad03
jihoseo <ssohye@icloud.com>
2025-05-07 12:55:51 +0900

    etc: 친구 - No detailed description (original message: 친구)

--- frontend/app/src/main/java/kr/gachon/adigo/data/remote/friend/FriendRemoteDataSource.kt
+++ frontend/app/src/main/java/kr/gachon/adigo/data/remote/friend/FriendRemoteDataSource.kt
@@ -1,11 +1,14 @@
 package kr.gachon.adigo.data.remote.friend
 
 import kr.gachon.adigo.data.remote.core.safeCall
+import retrofit2.Response
+import retrofit2.http.GET
 
 class FriendRemoteDataSource  (
     private val api: FriendApi
 ) {
     suspend fun friendList() = safeCall { api.friendList() }
     suspend fun deleteFriend(friendEmail: String) = safeCall { api.deleteFriend(friendEmail) }
+    suspend fun addFriend(friendEmail: String) = safeCall { api.addFriend(friendEmail) }
 
 }
