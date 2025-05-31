a8ba0fb87e60630abe2564086755e7ab71b1b684
jihoseo <ssohye@icloud.com>
2025-05-31 17:48:55 +0900

    회원가입창에서 생기던 에러 해결

--- frontend/app/src/main/java/kr/gachon/adigo/data/model/dto/SignUpResponse.kt
+++ frontend/app/src/main/java/kr/gachon/adigo/data/model/dto/SignUpResponse.kt
@@ -3,9 +3,6 @@ package kr.gachon.adigo.data.model.dto
 data class SignUpResponse(
     val status: Int,
     val message: String,
-    val data: Response
-) {
-    data class Response(
-        val isSuccess: Boolean,
-    )
-}
+    val data: Boolean
+)
+
